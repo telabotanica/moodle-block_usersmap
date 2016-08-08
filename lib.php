33972f35c8288e9553581f6b3905ec5016c6e55c
Mathias Chouet <mathias@tela-botanica.org>
2016-08-08 14:44:24 +0200

    Disabled whell scroll zoom to prevent focus grabbing surprises when scrolling on the page

--- lib.php
+++ lib.php
@@ -85,7 +85,9 @@ function usersmap_generate_content($config) {
             break;
     }
 
-    $jsinitmapcode .= "var usersmap = L.map('usersmap-map', {fullscreenControl: true, fullscreenControlOptions: {position: 'topleft'}});" . PHP_EOL;
+    $jsinitmapcode .= "var usersmap = L.map('usersmap-map', {scrollWheelZoom: false, fullscreenControl: true,
+                                      fullscreenControlOptions: {position: 'topleft'}});" . PHP_EOL;
+    //$jsinitmapcode .= "usersmap.once('focus', function() { usersmap.scrollWheelZoom.enable(); });" . PHP_EOL;
     $jsinitmapcode .= "baseLayer.addTo(usersmap);" . PHP_EOL;
     // Clustering layer.
     $jsinitmapcode .= "usersLayer = new L.MarkerClusterGroup({disableClusteringAtZoom : 10});";
